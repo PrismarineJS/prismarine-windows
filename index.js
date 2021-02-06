668ba19d40228d309dbd0c86947b9be25b6e6bfe
Karang <arthur.hennequin@free.fr>
2021-02-06 10:42:05 +0100

    Fix 1.16 wrong ids, add utility functions (#19)
    * Fix 1.16 wrong ids, add utility functions
    
    * Update API

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 function loader (mcVersion) {
   const Item = require('prismarine-item')(mcVersion)
   const Window = require('./lib/Window')(Item)
+  const mcData = require('minecraft-data')(mcVersion)
 
   let windows
-  if (mcVersion.startsWith('1.14') || mcVersion.startsWith('1.15') || mcVersion.startsWith('1.16')) {
+  if (mcData.isNewerOrEqualTo('1.14')) {
     // https://wiki.vg/Inventory
-    windows = {
-      'minecraft:inventory': { type: -1, inventory: { start: 9, end: 44 }, slots: 46, craft: 0, requireConfirmation: true },
-      'minecraft:generic_9x1': { type: 0, inventory: { start: 1 * 9, end: 1 * 9 + 35 }, slots: 1 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_9x2': { type: 1, inventory: { start: 2 * 9, end: 2 * 9 + 35 }, slots: 2 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_9x3': { type: 2, inventory: { start: 3 * 9, end: 3 * 9 + 35 }, slots: 3 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_9x4': { type: 3, inventory: { start: 4 * 9, end: 4 * 9 + 35 }, slots: 4 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_9x5': { type: 4, inventory: { start: 5 * 9, end: 5 * 9 + 35 }, slots: 5 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_9x6': { type: 5, inventory: { start: 6 * 9, end: 6 * 9 + 35 }, slots: 6 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:generic_3x3': { type: 6, inventory: { start: 7 * 9, end: 7 * 9 + 35 }, slots: 7 * 9 + 36, craft: -1, requireConfirmation: true },
-      'minecraft:anvil': { type: 7, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:beacon': { type: 8, inventory: { start: 1, end: 36 }, slots: 37, craft: -1, requireConfirmation: true },
-      'minecraft:blast_furnace': { type: 9, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:brewing_stand': { type: 10, inventory: { start: 5, end: 40 }, slots: 41, craft: -1, requireConfirmation: true },
-      'minecraft:crafting': { type: 11, inventory: { start: 10, end: 45 }, slots: 46, craft: 0, requireConfirmation: true },
-      'minecraft:enchantment': { type: 12, inventory: { start: 2, end: 37 }, slots: 38, craft: -1, requireConfirmation: true },
-      'minecraft:furnace': { type: 13, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:grindstone': { type: 14, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:hopper': { type: 15, inventory: { start: 5, end: 40 }, slots: 41, craft: -1, requireConfirmation: true },
-      'minecraft:lectern': { type: 16, inventory: { start: 1, end: 36 }, slots: 37, craft: -1, requireConfirmation: true },
-      'minecraft:loom': { type: 17, inventory: { start: 4, end: 39 }, slots: 40, craft: 3, requireConfirmation: true },
-      'minecraft:merchant': { type: 18, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:shulker_box ': { type: 19, inventory: { start: 27, end: 62 }, slots: 63, craft: -1, requireConfirmation: true },
-      'minecraft:smoker': { type: 20, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:cartography': { type: 21, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
-      'minecraft:stonecutter': { type: 22, inventory: { start: 2, end: 37 }, slots: 38, craft: 1, requireConfirmation: true }
+    windows = {}
+    let protocolId = -1
+    windows['minecraft:inventory'] = { type: protocolId++, inventory: { start: 9, end: 44 }, slots: 46, craft: 0, requireConfirmation: true }
+    windows['minecraft:generic_9x1'] = { type: protocolId++, inventory: { start: 1 * 9, end: 1 * 9 + 35 }, slots: 1 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_9x2'] = { type: protocolId++, inventory: { start: 2 * 9, end: 2 * 9 + 35 }, slots: 2 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_9x3'] = { type: protocolId++, inventory: { start: 3 * 9, end: 3 * 9 + 35 }, slots: 3 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_9x4'] = { type: protocolId++, inventory: { start: 4 * 9, end: 4 * 9 + 35 }, slots: 4 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_9x5'] = { type: protocolId++, inventory: { start: 5 * 9, end: 5 * 9 + 35 }, slots: 5 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_9x6'] = { type: protocolId++, inventory: { start: 6 * 9, end: 6 * 9 + 35 }, slots: 6 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:generic_3x3'] = { type: protocolId++, inventory: { start: 7 * 9, end: 7 * 9 + 35 }, slots: 7 * 9 + 36, craft: -1, requireConfirmation: true }
+    windows['minecraft:anvil'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:beacon'] = { type: protocolId++, inventory: { start: 1, end: 36 }, slots: 37, craft: -1, requireConfirmation: true }
+    windows['minecraft:blast_furnace'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:brewing_stand'] = { type: protocolId++, inventory: { start: 5, end: 40 }, slots: 41, craft: -1, requireConfirmation: true }
+    windows['minecraft:crafting'] = { type: protocolId++, inventory: { start: 10, end: 45 }, slots: 46, craft: 0, requireConfirmation: true }
+    windows['minecraft:enchantment'] = { type: protocolId++, inventory: { start: 2, end: 37 }, slots: 38, craft: -1, requireConfirmation: true }
+    windows['minecraft:furnace'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:grindstone'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:hopper'] = { type: protocolId++, inventory: { start: 5, end: 40 }, slots: 41, craft: -1, requireConfirmation: true }
+    windows['minecraft:lectern'] = { type: protocolId++, inventory: { start: 1, end: 36 }, slots: 37, craft: -1, requireConfirmation: true }
+    windows['minecraft:loom'] = { type: protocolId++, inventory: { start: 4, end: 39 }, slots: 40, craft: 3, requireConfirmation: true }
+    windows['minecraft:merchant'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:shulker_box'] = { type: protocolId++, inventory: { start: 27, end: 62 }, slots: 63, craft: -1, requireConfirmation: true }
+    if (mcData.isNewerOrEqualTo('1.16')) {
+      windows['minecraft:smithing'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
     }
+    windows['minecraft:smoker'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:cartography'] = { type: protocolId++, inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true }
+    windows['minecraft:stonecutter'] = { type: protocolId++, inventory: { start: 2, end: 37 }, slots: 38, craft: 1, requireConfirmation: true }
   } else {
     // https://wiki.vg/index.php?title=Inventory&oldid=14093
     windows = {
-      'minecraft:inventory': { type: 'minecraft:inventory', inventory: { start: 9, end: 44 }, slots: mcVersion.startsWith('1.8') ? 45 : 46, craft: 0, requireConfirmation: true },
+      'minecraft:inventory': { type: 'minecraft:inventory', inventory: { start: 9, end: 44 }, slots: mcData.isNewerOrEqualTo('1.9') ? 46 : 45, craft: 0, requireConfirmation: true },
       'minecraft:chest': null,
       'minecraft:crafting_table': { type: 'minecraft:crafting_table', inventory: { start: 10, end: 45 }, slots: 46, craft: 0, requireConfirmation: true },
       'minecraft:furnace': { type: 'minecraft:furnace', inventory: { start: 3, end: 38 }, slots: 39, craft: 2, requireConfirmation: true },
